1af85c064292d42951aa8637251ec7169c6304b2
Eszter K <73884228+banana0000@users.noreply.github.com>
2021-05-16 19:51:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -16,14 +16,14 @@ function colorClock(){
   }
 
   var clockFace = hours + ':' + minutes + ':' + seconds;
-  var hexaColor = 'hsl' + hours + minutes + seconds;
+  var hexaColor = 'rgb' + hours + minutes + seconds;
 
   document.getElementById('clock').innerHTML = clockFace;
   document.body.style.background = hexaColor;
 
   setTimeout(function() {
     colorClock();
-  }, 1000);
+  }, 5000);
 }
 
 colorClock();
